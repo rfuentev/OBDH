7c344f4db396af90d48e981fa9cfcba6fbf546d0
atcsol <atcsol@vmatc>
2024-06-02 12:45:42 +0200

    UAHExplorer

--- asw_pus_19_x_rtems_project_2024_students/service_libraries/emu_tc_programming/src/emu_tc_programming.cpp
+++ asw_pus_19_x_rtems_project_2024_students/service_libraries/emu_tc_programming/src/emu_tc_programming.cpp
@@ -24,9 +24,67 @@
 #define FT_SOLO_EPD_DeviceControl_0080
 
 //TODO
-//#define FT_SOLO_EPD_Event_Action_0090
-//#define FT_SOLO_EPD_Event_Action_0100
-//#define FT_SOLO_EPD_Event_Action_0110
+// NOMBRE DEL SERVICIO A USAR
+#ifdef FT_SOLO_EPD_ICU_SERV19_0190
+// Test para comprobar el servicio 19
+// Me defino los tiempos para los que van a ir ejecutando los steps
+#define FT_0190_TIME_step0(UNITIME_AFTER_POWER_ON + )
+
+// sinceramente estos puntos no sé para que sirven
+#define FT_SOLO_EPD_Event_Action_0090
+#define FT_SOLO_EPD_Event_Action_0100
+#define FT_SOLO_EPD_Event_Action_0110
+
+// PASO 0
+EmuGSS_TCProgram20_3 prog_FT_0190_step0(UNITIME_AFTER_POWER_ON + 5,"FT_SOLO_EPD_ICU_SERV_20_0190 STEP 0, Set Param 0 to value 7",0,7);
+// Me tiene que devolver unas determinadas TMs que son la (1,1) y la TM(1,7)
+// Como es emuTCprogramming no hay que programar las telemetrias que se reciben
+
+// PASO 1
+// Mandar un TC(12,5) para definir la monitorización del parámetro 0
+EmuGSS_TCProgram12_5 prog_FT_0190_step1(UNITIME_AFTER_POWER_ON + 6, "FT_SOLO_EPD_ICU_SERV_12_0190 STEP 1, Define the monitoring of parameter 0",0,1,0x3001,5,0x3002,10);
+
+// PASO 2
+// Permitir la monitorización de un TC(12,1)
+EmuGSS_TCProgram12_1 prog_FT_0190_step2(UNITIME_AFTER_POWER_ON + 7, "FT_SOLO_EPD_ICU_SERV_12_0190 STEP 2, Enable motoring sending",0);
+
+// PASO 3
+// Mandar un TC(19,1) para asociar la TC(128,2) al evento de 0x3002
+EmuGSS_TCProgram19_1 prog_FT_0190_step3(UNITIME_AFTER_POWER_ON + 8,  "FT_SOLO_EPD_ICU_SERV_12_0190 STEP 3, Send TC(19,1) to associate TC(128,2) to a type of event", 0x3002, 128,2);
+
+// PASO 4
+// Send TC(19,4) para permitir la event association
+EmuGSS_TCProgram19_4 prog_FT_0190_step4(UNITIME_AFTER_POWER_ON + 9, "FT_SOLO EPD_ICU_SERV_19_0190 STEP 4, Send TC(19,4) to enable event association", 0x3002);
+
+// PASO 5
+// Cambio el valor de un determinado parámetro para settearlo de 0 a 15, por eso uso servicio 20
+EmuGSS_TCProgram20_3 prog_FT_0190_step5(UNITIME_AFTER_POWER_ON + 10,"FT_SOLO_EPD_ICU_SERV_20_0190 STEP 5, Set Param 0 to value 15",0,15);
+
+// PASO 6
+// Ahora lo que hacemos es coger el valor del TC(19,5) to disable event association
+EmuGSS_TCProgram19_5 prog_FT_0190_step6(UNITIME_AFTER_POWER_ON + 11, "FT_SOLO _EPD_ICU_SERV_19_0190 STEP 6, Send TC(19,5) to disable event association", 0x3002);
+
+// PASO 7
+//Enviar un TC(20,3) para settear el parametro 0 a 15, cambio de valor
+EmuGSS_TCProgram20_3 prog_FT_0190_step7(UNITIME_AFTER_POWER_ON + 12,"FT_SOLO_EPD_ICU_SERV_20_0190 STEP 7, Set Param 0 to value 15",0,15);
+
+// PASO 8
+//Enviar TC(19,4) para permitir otra vez la event association
+// ni idea de porq hace esto????
+EmuGSS_TCProgram19_4 prog_FT_0190_step8(UNITIME_AFTER_POWER_ON + 13, "FT_SOLO EPD_ICU_SERV_19_0190 STEP 8, Send TC(19,4) to enable event association", 0x3002);
+
+
+// PASO 9
+// Enviar un TC(19,2) para borrar la asociacion de TC(128,2) al evento que habiamos puesto
+EmuGSS_TCProgram19_2 prog_FT_0190_step9(UNITIME_AFTER_POWER_ON + 14,  "FT_SOLO_EPD_ICU_SERV_12_0190 STEP 3, Send TC(19,2) to delete the association of TC (128,2) to event 0x3002", 0x3002);
+
+// PASO 10
+// Enviar un TC(20,3) para otra vez settear el parametro 0 a 15. Es posible que lo este cambiando??
+EmuGSS_TCProgram20_3 prog_FT_0190_step7(UNITIME_AFTER_POWER_ON + 15, "FT_SOLO_EPD_ICU_SERV_20_0190 STEP 7, Set Param 0 to value 15",0,15)
+#endif
+
+
+
 
 #ifdef FT_SOLO_EPD_ICU_SERV_17_0010
 
@@ -123,6 +181,7 @@ EmuGSS_TCProgram2_1 prog_FT_0080_step_2(UNITIME_AFTER_POWER_ON + 6,
 					6, DeviceCommandOn);
 
 #endif
+
 
 #ifdef FT_SOLO_EPD_Event_Action_0090
 
